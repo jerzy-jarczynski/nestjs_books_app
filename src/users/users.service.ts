9684d324b92ffa4a97a683a1d354f87c4391260f
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-09-20 13:47:28 +0200

    Add module, service, controller for auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -73,7 +73,7 @@ export class UsersService {
         where: { id },
         data: {
           ...userData,
-        }
+        },
       });
     } catch (error) {
       if (error.code === 'P2025') {
