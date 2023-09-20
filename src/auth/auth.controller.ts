8352d69882e3073da228dee0b3bbe825b0a69435
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-09-20 14:30:27 +0200

    Add match.decorator, add auth dto, add register endpoint, add register service method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDTO } from './dtos/register.dto';
+
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
+
+  @Post('/register')
+  register(@Body() registerData: RegisterDTO) {
+    return this.authService.register(registerData);
+  }
 }
