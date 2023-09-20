127cc8abe3748c7a025f0c25372e2a42680cd3c2
Jerzy Jarczynski <jerzy.jarczynski.95@gmail.com>
2023-09-20 13:22:25 +0200

    Add methods for users service. Add endpoints for users controller.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  ParseUUIDPipe,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get('/')
+  getAll(): any {
+    return this.usersService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    if (!(await this.usersService.getById(id))) {
+      throw new NotFoundException('User not found');
+    }
+    return this.usersService.getById(id);
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { PrismaService } from 'src/shared/services/prisma.service';
+import { Password, User } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
   constructor(private prismaService: PrismaService) {}
+
+  public getAll(): Promise<User[]> {
+    return this.prismaService.user.findMany();
+  }
+
+  public getById(id: User['id']): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { id },
+    });
+  }
+
+  public getByEmail(email: string): Promise<User | null> {
+    return this.prismaService.user.findUnique({
+      where: { email },
+      include: { password: true },
+    });
+  }
+
+  public async create(
+    userData: Omit<User, 'id' | 'role'>,
+    password: Password['hashedPassword'],
+  ): Promise<User> {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...userData,
+          password: {
+            create: {
+              hashedPassword: password,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('Email is already exist');
+      }
+      throw error;
+    }
+  }
+
+  public async updateById(
+    id: User['id'],
+    userData: Omit<User, 'id' | 'role'>,
+    password: string | undefined,
+  ): Promise<User> {
+    try {
+      if (password !== undefined) {
+        return await this.prismaService.user.update({
+          where: { id },
+          data: {
+            ...userData,
+            password: {
+              update: {
+                hashedPassword: password,
+              },
+            },
+          },
+        });
+      }
+      return await this.prismaService.user.update({
+        where: { id },
+        data: {
+          ...userData,
+        }
+      });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new BadRequestException("User doesn't exist");
+      } else if (error.code === 'P2002') {
+        throw new ConflictException('Email is already exist');
+      }
+      throw error;
+    }
+  }
+
+  public delete(id: User['id']): Promise<User> {
+    return this.prismaService.user.delete({
+      where: { id },
+    });
+  }
 }
